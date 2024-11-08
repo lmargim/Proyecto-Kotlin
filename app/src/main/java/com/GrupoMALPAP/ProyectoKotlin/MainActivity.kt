95bb20caa43258ee5a9eaed09c69f911d66ce352
Manuel <xmsanmar318@ieshnosmachado.org>
2024-11-08 09:30:15 +0100

    Commit con la navegacion intento solucion error 3

--- app/src/main/java/com/GrupoMALPAP/ProyectoKotlin/MainActivity.kt
+++ app/src/main/java/com/GrupoMALPAP/ProyectoKotlin/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.GrupoMALPAP.ProyectoKotlin.navegacion.Navegacion
-import com.manuelsantos.ejemplosnavigation.ui.theme.EjemplosNavigationTheme
+import com.GrupoMALPAP.ProyectoKotlin.ui.theme.ProyectoKotlinTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            EjemplosNavigationTheme {
+            ProyectoKotlinTheme {
                 Navegacion()
             }
         }
